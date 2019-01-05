7b5b577931844b868446a3a903d3eac6eb326e7f
Howard Q <afreefish@gmail.com>
2019-01-05 10:11:44 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Printer settings (Epson l360)
+sudo apt install cups cups-filters lsb git automake autoconf libtool libcups2-dev libcupsimage2 libcupsimage2-dev ghostscript
+sudo mkdir /opt/epson-inkjet-printer-201401w/
+sudo mkdir /opt/epson-inkjet-printer-201401w/ppds
+sudo mkdir /opt/epson-inkjet-printer-201401w/cups
+sudo mkdir /opt/epson-inkjet-printer-201401w/cups/lib
+sudo mkdir /opt/epson-inkjet-printer-201401w/cups/lib/filter
