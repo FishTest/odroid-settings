665bd05d02430a1f7759069a4b2d25c84d73e8d8
Howard Q <afreefish@gmail.com>
2019-01-05 11:46:39 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ sudo mkdir /opt/epson-inkjet-printer-201401w/ppds
 sudo mkdir /opt/epson-inkjet-printer-201401w/cups
 sudo mkdir /opt/epson-inkjet-printer-201401w/cups/lib
 sudo mkdir /opt/epson-inkjet-printer-201401w/cups/lib/filter
+#usb automount
+sudo blkid
+sudo nano /etc/fstab
+UUID=Replace here with blkid /media/Data1 ntfs defaults 0 0
+UUID=Replace here with blkid /media/Data2 ntfs defaults 0 0
