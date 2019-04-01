15c94bdd170b8ab75749a7f816f8c89525df7dbb
FishTest <afreefish@gmail.com>
2019-04-01 14:25:16 +0000

    bug fix

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #Printer settings (Epson l360)
-sudo apt install cups cups-filters lsb git automake autoconf libtool libcups2-dev libcupsimage2 libcupsimage2-dev ghostscript
-sudo mkdir /opt/epson-inkjet-printer-201401w/
-sudo mkdir /opt/epson-inkjet-printer-201401w/ppds
-sudo mkdir /opt/epson-inkjet-printer-201401w/cups
-sudo mkdir /opt/epson-inkjet-printer-201401w/cups/lib
-sudo mkdir /opt/epson-inkjet-printer-201401w/cups/lib/filter
+apt install cups cups-filters lsb git automake autoconf libtool libcups2-dev libcupsimage2 libcupsimage2-dev ghostscript
+mkdir /opt/epson-inkjet-printer-201401w/
+mkdir /opt/epson-inkjet-printer-201401w/ppds
+mkdir /opt/epson-inkjet-printer-201401w/cups
+mkdir /opt/epson-inkjet-printer-201401w/cups/lib
+mkdir /opt/epson-inkjet-printer-201401w/cups/lib/filter
+cp opt/epson-inkjet-printer-201401w/cups/lib/filter/epson_inkjet_printer_filter /opt/epson-inkjet-printer-201401w/cups/lib/filter/
+cp opt/epson-inkjet-printer-201401w/ppds/EPSON_L360.ppd /opt/epson-inkjet-printer-201401w/ppds/
+
 #usb automount
-sudo blkid
-sudo nano /etc/fstab
-UUID=Replace here with blkid /media/Data1 ntfs defaults 0 0
-UUID=Replace here with blkid /media/Data2 ntfs defaults 0 0
+blkid
+#sudo nano /etc/fstab
+#UUID=Replace here with blkid /media/Data1 ntfs defaults 0 0
+#UUID=Replace here with blkid /media/Data2 ntfs defaults 0 0
